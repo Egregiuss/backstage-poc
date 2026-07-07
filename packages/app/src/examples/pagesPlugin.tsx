77f87df26355eac02e9483907a0611ea918c0158
Olumuyiwa <olumuyiwa.folorunso@gmail.com>
2026-07-07 11:40:02 +0100

    chore: yarn tsc fix, removed BreadcrumbEntry

--- packages/app/src/examples/pagesPlugin.tsx
+++ packages/app/src/examples/pagesPlugin.tsx
@@ -26,7 +26,6 @@ import {
   createExtensionBlueprint,
   createExtensionInput,
   coreExtensionData,
-  BreadcrumbEntry,
 } from '@backstage/frontend-plugin-api';
 import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
@@ -171,20 +170,14 @@ const Page1 = PageBlueprint.make({
                   <Route
                     path="/"
                     element={
-                      <BreadcrumbEntry entry={{ label: 'Page 1', href: '/' }}>
-                        <h2>This is also page 1</h2>
-                      </BreadcrumbEntry>
+                        <h2>This is also page 1</h2>    
                     }
                   />
 
                   <Route
                     path="/page2"
                     element={
-                      <BreadcrumbEntry
-                        entry={{ label: 'Page 2', href: '/page2' }}
-                      >
                         <h2>This is page 2</h2>
-                      </BreadcrumbEntry>
                     }
                   />
                 </Routes>
